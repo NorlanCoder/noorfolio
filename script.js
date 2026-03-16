5120a87650b6092455c31163df486f4e426e91cb
NorlanCoder <fabien.amoussou20@gmail.com>
2026-03-16 16:02:09 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// Version simple avec un PDF fixe : Memoire_Licence.pdf
+// Ce script est optionnel ici, mais on peut s'en servir plus tard
+// si tu veux ajouter d'autres interactions (changer de document, zoom, etc.).
